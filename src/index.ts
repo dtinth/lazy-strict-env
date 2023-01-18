69474b6f104b9a8527d4429c88d5594fadf12feb
dtinth on MBP M1 <dtinth@spacet.me>
2023-01-18 18:26:46 +0700

    Update API docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import type { ZodTypeAny, z } from 'zod'
 
+/**
+ * Registers a lazily-validated schema on the environment variables
+ * (`process.env`, or a custom object specified by the `target` parameter).
+ *
+ * @remarks
+ *  Validation is done the first time an environment variable is accessed.
+ *  Environment variables that is not used at runtime will not be validated.
+ *
+ * @param spec - A map of environment variable names to Zod schemas.
+ * @param target - The object to register the environment variables on
+ *  (defaults to `process.env`)
+ * @returns The environment variables object (`process.env` or `target`).
+ * @public
+ */
 export function Env<T extends Record<string, ZodTypeAny>>(
   spec: T,
   target: Record<string, string> = process.env as any,
@@ -35,8 +49,21 @@ class EnvProxy<T> {
   }
 }
 
+/**
+ * Takes a mapped type of Zod schemas and returns a mapped type of the inferred types.
+ * @public
+ */
 export type InferProps<T extends Record<string, ZodTypeAny>> = {
   [K in keyof T]: T[K] extends ZodTypeAny ? z.infer<T[K]> : never
 }
 
+/**
+ * Infers the object type based on the return value of {@link (Env:function)}.
+ * Use it to augment the global `NodeJS.ProcessEnv` interface.
+ *
+ * @remarks
+ *  This type simply resolves to itself to work around a TypeScript limitation
+ *  where {@link https://github.com/Microsoft/TypeScript/issues/18271 | `typeof` cannot be used in the `implements` clause}.
+ * @public
+ */
 export type Env<T extends Record<string, any>> = T
